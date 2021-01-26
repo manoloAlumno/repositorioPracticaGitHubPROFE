dd334e7ae0740e5f9b614f67d2d2541a2137cbb4
juananPROFE <juanantoniolm@gmail.com>
2021-01-26 19:29:21 +0100

    Añadido Syso a Saluda.java, Juanan

--- proyectoGit/Saluda.java
+++ proyectoGit/Saluda.java
@@ -4,5 +4,6 @@ public class Saluda{
 
 	public static void main(String[] args){
 		System.out.println("Hola chavales, ¿cómo estáis?");
+		System.out.println("Estoy mu bien.Soy Juan Antonio López Mera");
 	}
 }
