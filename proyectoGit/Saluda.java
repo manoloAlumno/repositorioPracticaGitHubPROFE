75c2ef7e69d0473c5b4d87ffc7a45344b0688679
antoniobarrera1 <antonio.barrera@iesvelazquez.org>
2021-03-01 17:34:01 +0100

    Añadiendo syso suplicante, ANTONIO

--- proyectoGit/Saluda.java
+++ proyectoGit/Saluda.java
@@ -5,5 +5,7 @@ public class Saluda{
 	public static void main(String[] args){
 		System.out.println("Hola chavales, ¿cómo estáis?");
 		System.out.println("Estoy mu bien.Soy Juan Antonio López Mera");
+		System.out.println("Estoy mu bien.Soy Antonio Barrera Romero");
+		System.out.println("Yo no suplico por dieces, suplico por cincos");
 	}
 }
