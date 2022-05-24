8668b898ee5977dd4c2d35215fffe38030f27440
Taky92 <trodcar851@g.educaand.es>
2022-05-24 13:50:37 +0200

    Añadiendo syso saludando

--- proyectoGit/Saluda.java
+++ proyectoGit/Saluda.java
@@ -12,6 +12,6 @@ public class Saluda{
 		System.out.println("Hola, ¿que tal?. Jesus Ramirez Richarte");
 		System.out.println("Hola, estoy muy bien. Soy Ariana Martín Martínez");
 		System.out.println("Aquí Raúl Aguilar García, necesito unas vacaciones :D");
-
+                System.out.println("Holita, Juan. Soy Tania Rodriguez. Taky para los amigos ;)")
 	}
 }
